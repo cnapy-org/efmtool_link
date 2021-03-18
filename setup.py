8263a2e0e1eb590c8a19a406a3ed12d5d594006c
Sven Thiele <sthiele78@gmail.com>
2021-03-18 09:47:37 +0100

    add jar files

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='efmtool_link',
-    version='0.0.1',
-    description='Exposes various efmtool functionality in Python.',
-    url='https://github.com/cnapy-org/efmtool_link.git',
-    author='Axel von Kamp',
-    author_email='axelk1@gmx.de',
-    license='Apache License 2.0',
-    packages=['efmtool_link'],
-    zip_safe=False)
+      version='0.0.1',
+      description='Exposes various efmtool functionality in Python.',
+      url='https://github.com/cnapy-org/efmtool_link.git',
+      author='Axel von Kamp',
+      author_email='axelk1@gmx.de',
+      license='Apache License 2.0',
+      packages=['efmtool_link'],
+      package_data={'efmtool_link': ['lib/*.jar']},
+      zip_safe=False)
