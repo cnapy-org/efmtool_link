153cfb8af27198dc7e07517d99e76c40969f7016
axelvonkamp <axelk1@gmx.de>
2024-08-19 15:25:41 +0200

    PyPI support

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 from setuptools import setup
 
 setup(name='efmtool_link',
-      version='0.0.6',
-      description='Exposes various efmtool functionality in Python.',
-      url='https://github.com/cnapy-org/efmtool_link.git',
-      author='Axel von Kamp',
-      author_email='axelk1@gmx.de',
-      license='Apache License 2.0',
       packages=['efmtool_link'],
+      package_dir={'efmtool_link': 'efmtool_link'},
       package_data={'efmtool_link': ['lib/*.jar']},
-      install_requires=['jpype1', 'numpy', 'scipy', 'cobra', 'psutil'],
       zip_safe=False)
